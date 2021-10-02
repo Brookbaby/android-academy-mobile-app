a840d577d2a9ba0c5abb7c2caaa3635e385d3549
Alexey Ignatyev <aleksei.ignatyev@joy-dev.com>
2021-10-02 18:47:16 +0300

    верстала экраны и создала функцию перехода

--- app/src/main/java/com/example/academyapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/academyapp/RegistrationActivity.kt
@@ -1,21 +1,43 @@
 package com.example.academyapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.academyapp.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistrationBinding
+    var fragmentId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
+
+        navigation()
+
         setContentView(binding.root)
+    }
+
+    private fun navigation() {
+        with(binding) {
+            registrationSwitch.setOnClickListener {
+                fragmentId = findNavController(R.id.fragmentContainerView).currentDestination?.id ?: 0
+                if (fragmentId != R.id.registrationFragment) {
+                    findNavController(R.id.fragmentContainerView).navigate(R.id.action_loginFragment_to_registrationFragment)
+                    loginSwitch.setTextColor(resources.getColor(R.color.nice_blue))
+                    registrationSwitch.setTextColor(resources.getColor(R.color.white))
+                }
+            }
+
+            loginSwitch.setOnClickListener {
+                fragmentId = findNavController(R.id.fragmentContainerView).currentDestination?.id ?: 0
+                if (fragmentId != R.id.loginFragment) {
+                    findNavController(R.id.fragmentContainerView).navigate(R.id.action_registrationFragment_to_loginFragment)
+                    registrationSwitch.setTextColor(resources.getColor(R.color.nice_blue))
+                    loginSwitch.setTextColor(resources.getColor(R.color.white))
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/academyapp/RegistrationFragment.kt
+++ app/src/main/java/com/example/academyapp/RegistrationFragment.kt
@@ -1,26 +1,22 @@
 package com.example.academyapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.academyapp.databinding.FragmentSecondBinding
+import androidx.fragment.app.Fragment
+import com.example.academyapp.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentRegistrationBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+        _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
